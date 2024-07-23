c1dadb44ddc1bcdefa3a0fe72f024db5d3d6c4be
Maxwell Morrissey <mmorrissey5961@gmail.com>
2024-07-22 22:24:41 -0400

    Cleanup

--- lib/types.ts
+++ lib/types.ts
@@ -1,16 +1,8 @@
 import http from "http";
 import HttpResponse from "./HttpResponse";
 
-/* The Request.url methods */
-// export type RequestMethod = "GET" | "POST" | "PUT" | "DELETE";
-
 /* The app[method] names */
 export type AppMethod = "GET" | "POST" | "PUT" | "DELETE" | "USE";
-
-/* These are the types used to start the lifecycle and will be promptly
- * extended or referred to by the the HttpResponse and HttpRequest types afterwards */
-// export type HttpResponse = http.ServerResponse;
-// export type HttpRequest = http.IncomingMessage;
 
 export type HttpRequest = http.IncomingMessage & {
     body?: any;
